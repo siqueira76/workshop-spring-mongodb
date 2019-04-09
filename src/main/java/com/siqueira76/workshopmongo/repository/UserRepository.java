1f4bdabe1979bc60ebe2dfc6e38fc5aff2239f16
José Carlos Siqueira <josecarlos.siqueira76@gmail.com>
2019-04-09 10:40:36 -0300

    Getting user by id

--- src/main/java/com/siqueira76/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/siqueira76/workshopmongo/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.siqueira76.workshopmongo.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.siqueira76.workshopmongo.domain.User;
 
+
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
 }
