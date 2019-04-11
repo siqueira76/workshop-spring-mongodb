71777137aba78f29fd9b7fc6285a12bee2395a1c
José Carlos Siqueira <josecarlos.siqueira76@gmail.com>
2019-04-11 08:42:48 -0300

    insert user by POST

--- src/main/java/com/siqueira76/workshopmongo/service/UserService.java
+++ src/main/java/com/siqueira76/workshopmongo/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.siqueira76.workshopmongo.domain.User;
+import com.siqueira76.workshopmongo.dto.UserDTO;
 import com.siqueira76.workshopmongo.repository.UserRepository;
 import com.siqueira76.workshopmongo.service.exception.ObjectNotFoundException;
 
@@ -23,7 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-		
+	}
+	
+	public User insert(User user) {
+		return repo.insert(user);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
