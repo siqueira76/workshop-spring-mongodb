9a87ae9ea4ee9620759d7159744922cdf1fc93fa
José Carlos Siqueira <josecarlos.siqueira76@gmail.com>
2019-04-17 17:18:32 -0300

    Update user with PUT

--- src/main/java/com/siqueira76/workshopmongo/resources/UserResource.java
+++ src/main/java/com/siqueira76/workshopmongo/resources/UserResource.java
@@ -51,5 +51,13 @@ public class UserResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build(); // retorna o codigo 204
 	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id){
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build(); // retorna o codigo 204
+	}
 
 }
